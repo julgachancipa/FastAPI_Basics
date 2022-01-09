69f3f7c61e7a9ec3e93b4b89fbf57a0deeefd62e
julgachancipa <julianaga01@gmail.com>
2022-01-08 19:25:49 -0500

    Validations Path parameters

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -17,21 +17,49 @@ class Person(BaseModel):
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
 
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
 
+
 # Request and Response Body
+
 
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
 
-# Validaciones: Qury parameters
+
+# Validations: Query parameters
+
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: Optional[str] = Query(...)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters",
+    ),
+    age: Optional[str] = Query(
+        ..., title="Peson Age", description="This is the person age. It's required"
+    ),
 ):
     return {name: age}
+
+
+# Validations: Path parameters
+
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person ID",
+        description="This is the person age. It's required",
+    )
+):
+    return {person_id: "It exists!"}
